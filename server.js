07758fcc0c5e2dfbe1d8e29f4cf223b22ebab576
Dagestan05 <steklofan@gmail.com>
2018-12-02 15:20:19 +0100

    added user model & user register post route

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users")
 const posts = require("./routes/api/posts")
 const profile = require("./routes/api/profile")
 
 const app = express();
+//bodyParser setup
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // DB config
-const db = require("./config/keys").MONGOURI || "mongodb://127.0.0.1:27017";
+const db = require("./config/keys").MONGOURI || "mongodb://127.0.0.1:27017/soc_set";
 mongoose
   .connect(db)
   .then(() => console.log("Mongo DB connected"))
@@ -17,6 +21,7 @@ mongoose
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
 // Use routes
 app.use("/api/users", users)
 app.use("/api/profile", profile)
