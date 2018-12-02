17d864d0896495bf573096025c1c763892fb3c1d
Dagestan05 <steklofan@gmail.com>
2018-12-02 03:06:17 +0100

    initial basic routes

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users")
+const posts = require("./routes/api/posts")
+const profile = require("./routes/api/profile")
 
 const app = express();
 
-app.get("/", (req, res)=>{
-  res.send("hello")
-})
+// DB config
+const db = require("./config/keys").MONGOURI || "mongodb://127.0.0.1:27017";
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongo DB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+// Use routes
+app.use("/api/users", users)
+app.use("/api/profile", profile)
+app.use("/api/posts", posts)
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-  console.log('Server running on port ', port);
-})
+app.listen(port, () => {
+  console.log("Server running on port ", port);
+});
