a0bb2fd4abaa64e54a46cdb00b39708e3861deba
Dagestan05 <steklofan@gmail.com>
2018-12-05 04:01:49 +0100

    added profile model&get current profile route

---
+++ models/Profile.js
@@ -0,0 +1,145 @@
+const mongoose = require("mongoose");
+
+// Create a Schema
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubUserName: {
+    type: String
+  },
+  experience: [
+      {
+        title:{
+          type: String,
+          required: true
+        }
+      },
+      {
+        company:{
+          type: String,
+          required: true
+        }
+      },
+      {
+        location:{
+          type: String
+        }
+      },
+      {
+        from:{
+          type: Date,
+          required: true
+        }
+      },
+      {
+        to:{
+          type: Date
+        }
+      },
+      {
+        current:{
+          type: Boolean,
+          default: true
+        }
+      },
+      {
+        description:{
+          type: String
+        }
+      },
+    ],
+  education: [
+      {
+        school:{
+          type: String,
+          required: true
+        }
+      },
+      {
+        degree:{
+          type: String,
+          required: true
+        }
+      },
+      {
+        fieldOfStudy:{
+          type: String,
+          required: true
+        }
+      },
+      {
+        from:{
+          type: Date,
+          required: true
+        }
+      },
+      {
+        to:{
+          type: Date
+        }
+      },
+      {
+        current:{
+          type: Boolean,
+          default: true
+        }
+      },
+      {
+        description:{
+          type: String
+        }
+      },
+    ],
+    socialM: {
+      youtube: {
+        type: String
+      },
+      twitter: {
+        type: String
+      },
+      facebook: {
+        type: String
+      },
+      linkedin: {
+        type: String
+      },
+      instagram: {
+        type: String
+      },
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    }
+})
+
+const Profile = mongoose.model("profile", profileSchema)
+
+module.exports = Profile;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 
 /**
  * @route   GET api/profile/test
  * @desc    tests profile route
  * @access  Public
  */
-router.get("/test", (req, res)=>{
+router.get("/test", (req, res) => {
   res.json({
     msg: "Pofile works"
-  })
-})
+  });
+});
+
+/**
+ * @route   GET api/profile
+ * @desc    get current users profile
+ * @access  Private
+ */
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "The is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
