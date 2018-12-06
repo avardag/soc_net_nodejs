75f83252fdf6a993973878e35989f0150fe7c98a
Dagestan05 <steklofan@gmail.com>
2018-12-06 04:13:54 +0100

    added profile routes: get all, by username, by handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -110,4 +110,69 @@ router.post(
   }
 );
 
+/**
+ * @route   GET api/profile/handle/:handle
+ * @desc    get profile by handle
+ * @access  Public
+ */
+
+ router.get("/handle/:handle", (req, res)=>{
+   const errors = {};
+
+   Profile.findOne({handle: req.params.handle})
+    .populate('user', ['name', 'avatar'])
+    .then(foundProfile=>{
+      if(!foundProfile){
+        errors.noprofile = "There is no profile for this user"
+        res.status(404).json(errors)
+      }
+      res.json(foundProfile)
+    })
+    .catch(err=> res.status(404).json(err))
+ })
+
+/**
+ * @route   GET api/profile/user/:user_id
+ * @desc    get profile by user id
+ * @access  Public
+ */
+
+ router.get("/user/:user_id", (req, res)=>{
+   const errors = {};
+
+   Profile.findOne({user: req.params.user_id})
+    .populate('user', ['name', 'avatar'])
+    .then(foundProfile=>{
+      if(!foundProfile){
+        errors.noprofile = "There is no profile for this user"
+        res.status(404).json(errors)
+      }
+      res.json(foundProfile)
+    })
+    .catch(err=> res.status(404).json({noprofile:"There is no profile for this user"}))
+ })
+
+/**
+ * @route   GET api/profile/all
+ * @desc    get all profiles
+ * @access  Public
+ */
+
+ router.get("/all", (req, res)=>{
+   const errors = {};
+
+   Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(foundProfiles=>{
+      if(!foundProfiles){
+        errors.noprofile = "There are no profiles"
+        res.status(404).json(errors)
+      }
+      res.json(foundProfiles)
+    })
+    .catch(err=> res.status(404).json({noprofile:"There are no profile"}))
+ })
+
+
+
 module.exports = router;
