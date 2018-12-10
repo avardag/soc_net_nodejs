c2be92a266cac8f02dfbdc3101d2610f28eafe6a
Dagestan05 <steklofan@gmail.com>
2018-12-10 04:14:04 +0100

    added redux, initial store setup w/ reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./App.css";
 //react router imports
 import { BrowserRouter, Route } from "react-router-dom";
+//Redux imports
+import { Provider } from 'react-redux';
+import store from "./store";
 //Components
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -9,21 +12,25 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
+
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route path="/" component={Landing} exact />
-          <div className="container">
-            <Route path="/register" component={Register} exact />
-            <Route path="/login" component={Login} exact />
-          </div>
+      <Provider store={ store }>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/register" component={Register} exact />
+              <Route path="/login" component={Login} exact />
+            </div>
 
-          <Footer />
-        </div>
-      </BrowserRouter>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state=initialState, action){
+  switch (action.type) {
+    // case SET_CURRENT_USER:
+    //   return state ;
+  
+    default:
+      return state;
+  }
+}
