89120c67c9329651d2b50c1d3ce8f798a54b977b
Dagestan05 <steklofan@gmail.com>
2018-12-15 05:17:44 +0100

    added functionality to delete CCOUNT

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from "./types"
+import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from "./types"
 
 //get current profile
 export const getCurrentProfile = () => dispatch=> {
@@ -23,6 +23,21 @@ export const createProfile = (profileData, thisPropsHistory) => dispatch =>{
       type: GET_ERRORS,
       payload: err.response.data
     }))
+}
+
+//DELETE account and profile
+export const deleteAccount = () => dispatch =>{
+  if(window.confirm("Are you sure you want to delete account?")){
+    axios.delete("/api/profile")
+      .then(res => dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      }))
+      .catch(err=> dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
+  }
 }
 
 //Profile loading
