e553a41cbdecb1a5bab54bc097df9e8dd5e360dc
Dagestan05 <steklofan@gmail.com>
2018-12-17 08:38:07 +0100

    added compnts to display education&experince in Dashboard, redux actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -45,6 +45,32 @@ export const addEducation = (eduData, thisPropsHistory) => dispatch =>{
     }))
 }
 
+//Delete experience
+export const deleteExperience = (id) => dispatch =>{
+  axios.delete(`/api/profile/experience/${id}`)
+    .then(res=> dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    }))
+    .catch(err=>dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }))
+}
+
+//Delete education
+export const deleteEducation = (id) => dispatch =>{
+  axios.delete(`/api/profile/education/${id}`)
+    .then(res=> dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    }))
+    .catch(err=>dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    }))
+}
+
 //DELETE account and profile
 export const deleteAccount = () => dispatch =>{
   if(window.confirm("Are you sure you want to delete account?")){
