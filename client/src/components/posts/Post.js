9c9e5055d6bbdf37590f1ce5ac4ed76f2a4bc1f2
Dagestan05 <steklofan@gmail.com>
2018-12-20 07:44:52 +0100

    created component, action&reducer to fetch and display posts

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -3,17 +3,29 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../common/Spinner';
 import PostForm from './PostForm';
-
-
+import { getPosts } from '../../actions/postActions';
+import PostFeed from './PostFeed';
 
 class Post extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+  
   render() {
+    const {posts, loading} = this.props.post;
+    let postContent;
+    if(posts === null || loading){
+      postContent = <Spinner/>
+    }else{
+      postContent = <PostFeed posts={posts} />
+    }
     return (
       <div className="feed">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <PostForm/>
+              {postContent}
             </div>
           </div>
         </div>
@@ -23,7 +35,12 @@ class Post extends Component {
 }
 
 Post.propTypes = {
-
+  post: PropTypes.object.isRequired,
+  getPosts: PropTypes.func.isRequired
 };
 
-export default Post;
+const mapStateToProps = (state) =>({
+  post: state.post
+})
+
+export default connect(mapStateToProps, { getPosts })(Post);
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST } from '../actions/types';
+import { ADD_POST, GET_POSTS, POST_LOADING } from "../actions/types";
 const initialState = {
   posts: [],
   post: {},
@@ -7,13 +7,22 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts]
-    };
-
+      };
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
     default:
       return state;
   }
